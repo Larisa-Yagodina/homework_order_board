ef228d67e11050ccf2afb9fd3975aabafee6dee0
Larisa-Yagodina <cheeseroll@mail.ru>
2021-01-23 06:13:48 +0300

    create order list

--- src/App.js
+++ src/App.js
@@ -1,25 +1,36 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import { v4 as uuidv4 } from 'uuid';
+import {useState} from "react/cjs/react.production.min";
+import 'bootstrap/dist/css/bootstrap.css';
+import OrderTable from "./OrderTable";
+
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+    const [clients, setClients] = useState([
+        {id: uuidv4(), name: 'Alice Smith', address: null, phone: ''},
+        {id: uuidv4(), name: 'Mary Smith', address: null, phone: ''},
+        {id: uuidv4(), name: 'Bill Brown', address: null, phone: ''},
+        {id: uuidv4(), name: 'Bob Adams', address: null, phone: ''},
+    ]);
+
+    const [services, setServices] = useState([
+        {id: uuidv4(), job: 'change oil', primeCost: 50, price: 200},
+        {id: uuidv4(), job: 'repair engine', primeCost: 100, price: 400},
+        {id: uuidv4(), job: 'change wheels', primeCost: 100, price: 500},
+    ])
+
+
+
+    return (
+        <div>
+
+            <h1> Hello </h1>
+
+
+
+        </div>
+    );
 }
 
 export default App;
